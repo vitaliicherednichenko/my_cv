451ef28d2087722beb9fea33925d55e6af2e2ea0
Vitaliy Cherednichenko <vitaliycherednichenko@Vitaliys-MacBook-Pro.local>
2026-04-04 14:50:16 +0200

    Add create actions for profile models

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -8,9 +8,9 @@ class Profile < ApplicationRecord
   has_many :certificates, dependent: :destroy
   has_many :projects, dependent: :destroy
 
-  accepts_nested_attributes_for :skills, allow_destroy: true
+  accepts_nested_attributes_for :skills, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :experiences, allow_destroy: true
-  accepts_nested_attributes_for :languages, allow_destroy: true
+  accepts_nested_attributes_for :languages, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :educations, allow_destroy: true
   accepts_nested_attributes_for :certificates, allow_destroy: true
   accepts_nested_attributes_for :projects, allow_destroy: true
